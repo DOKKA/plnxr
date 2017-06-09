9be0a99119c5b31badb9bfe80e8454841d387eea
Kevin Grieger <scuffmark@gmail.com>
2017-06-09 00:10:54 -0500

    added buy command

--- bin/plnxr.js
+++ bin/plnxr.js
@@ -41,4 +41,30 @@ var argv=yargs.usage('$0 <cmd> [args]')
     },function(argv){
         commands.buyCommand(argv);
     })
+    .command('sell [args]', 'sell some coins',function(yargs){
+        return yargs.option('currencyPair',{
+            alias: 'c',
+            demandOption: true,
+            describe: 'the currency pair to trade on',
+            type: 'string'
+        }).option('baseSellPercentage',{
+            alias: 'p',
+            demandOption: false,
+            describe: 'the percentage of your trade currency to use for selling',
+            type: 'number'
+        }).option('sellLimitPercentage',{
+            alias: 'l',
+            demandOption: false,
+            describe: 'the percentage higher you want to sell the currency for',
+            type: 'number'
+        }).option('panicSell',{
+            alias: 'panic',
+            demandOption: false,
+            describe: 'sell at a slightly lower price to ensure the order fills',
+            type: 'boolean'
+        });
+    
+    },function(argv){
+        commands.sellCommand(argv);
+    })
     .help().argv;
